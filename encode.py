435d2c9212d47354894145623ad8eebfe4e6d8a0
Randy Caamal <43390072+rcaamal@users.noreply.github.com>
2018-10-01 00:55:54 -0700

    added the encode.py file manually
    i got help with this code here is the url to where i got help https://stackoverflow.com/questions/32855812/create-a-compress-function-in-python

---
+++ encode.py
@@ -0,0 +1,16 @@
+def encode(input_string):
+  return ""
+
+
+def encode(string):
+    count=0
+
+    for i in range(0, len(s)):
+        
+        if string[i] == string[i-1]:
+            count += 1
+        c = string.count(string[i])
+        
+
+    return str(s[i]) + str(c)
+
